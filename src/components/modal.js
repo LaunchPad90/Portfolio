649da3d9b9e09658562b13f75ab5b64ccd7db096
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-05-18 04:22:20 -0600

    modal style

--- src/components/modal.js
+++ src/components/modal.js
@@ -5,12 +5,13 @@ const customStyles = {
   content : {
     top: '50%',
     left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-20%',
     transform: 'translate(-50%, -50%)',
     height: '70%',
-    width: '80%'
+    width: '80%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 };
 
@@ -27,16 +28,15 @@ const buttonStyle = {
 }
 
 function GifModal(props){
-  var subtitle;
+
   const [modalIsOpen,setIsOpen] = React.useState(false);
   function openModal() {
     setIsOpen(true);
   }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
+  // function afterOpenModal() {
+
+  // }
 
   function closeModal(){
     setIsOpen(false);
@@ -47,14 +47,13 @@ function GifModal(props){
         <button style={buttonStyle.btn} onClick={openModal}>View Demo</button>
         <Modal
           isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
+          // onAfterOpen={afterOpenModal}
           onRequestClose={closeModal}
           style={customStyles}
           contentLabel={props.title}
         >
 
-          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-          <button onClick={closeModal}>close</button>
+          <button style={{fontSize: `2rem`, alignSelf: `flex-end`, cursor: `pointer`}} onClick={closeModal}>X</button>
           <iframe style={{ height: `100%`, width: `100%`}} src={props.gif} title={props.gif}/>
         </Modal>
       </div>
